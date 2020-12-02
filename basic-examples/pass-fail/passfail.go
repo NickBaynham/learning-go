19dddf8e7dbd9153d1381302a64bd307adcb70c1
Nick Baynham <nick.baynham@oracle.com>
2020-12-02 17:30:02 -0500

    added example pass-fail

--- basic-examples/pass-fail/passfail.go
+++ basic-examples/pass-fail/passfail.go
@@ -5,11 +5,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"log"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Enter a grade:")
 	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(input)
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64) 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println(status)
 }
